31a56f7a73b5166030ef9c50788e446d9725b303
Victor-Jenkins <63927632+Victor-Jenkins@users.noreply.github.com>
2023-05-11 00:25:48 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="container">
+  <h1 style="color: orangered ;">Rick y Morty</h1>
+  <h2 style="color: orangered;"> Página creada por Victor Bañó con NodeJS, VUECLI, CSS, Javascript, HTML</h2>
+  <FilterByStatus />
+  <FilterByName />
+  <ListCharacters />
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListCharacters from '@/components/ListCharacters'
+import FilterByStatus from './components/FilterByStatus';
+import FilterByName from './components/FilterByName';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body:rgb(5, 0, 0);
+  --background-card:rgb(52, 245, 4);
+
+  --text-white:rgb(4, 10, 0);
+  --text-gray:rgb(138, 10, 10);
+  --text-orange:orange;
+
+}
+* {
+  box-sizing: border-box;
+  margin: 5px;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 5rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 980px;
+  max-width: 80%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
